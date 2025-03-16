15b4de4064f5a5e4403e5bba731f0b70cffbf687
박종범 <jbpark03@gmail.com>
2025-03-16 19:32:50 +0900

    how Supplier works - my demo

--- src/Func_Inter/JavaLib/HowSupplier.java
+++ src/Func_Inter/JavaLib/HowSupplier.java
@@ -1,4 +1,27 @@
 package Func_Inter.JavaLib;
 
-public class HowProvider {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class HowSupplier {
+    public static void main(String[] args) {
+        var howWork = new HowSupplier();
+        Supplier<List<Integer>> fiveInts = () -> {
+            List<Integer> intList = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                intList.add(getRandomNumber(0, 100));
+            }
+            return intList;
+        };
+        howWork.utilizeSupplier(fiveInts);
+    }
+
+    void utilizeSupplier(Supplier<List<Integer>> supplier) {
+        System.out.println(supplier.get());
+    }
+
+    static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
 }
